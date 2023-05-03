a2abe4df50a8995ba19722c6d4fdbe64422c1d20
TrashBandiHoot <112197685+TrashBandiHoot@users.noreply.github.com>
2023-05-03 18:53:04 -0500

    Snake class and screen

---
+++ snake.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+
+STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+
+    def create_snake(self):
+        for pos in STARTING_POS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(pos)
+            self.segments.append(new_segment)
+
+    
+    def move(self):
+        for i in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[i - 1].xcor()
+            new_y = self.segments[i - 1].ycor()
+            self.segments[i].goto(new_x, new_y)
+        
+        self.segments[0].forward(20)
