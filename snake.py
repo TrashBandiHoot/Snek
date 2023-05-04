38e27b93ac81cecd74fe47661b76471b9bad2525
TrashBandiHoot <112197685+TrashBandiHoot@users.noreply.github.com>
2023-05-03 19:09:27 -0500

    added snake controls

--- snake.py
+++ snake.py
@@ -2,12 +2,18 @@ from turtle import Turtle
 
 
 STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
 
     def create_snake(self):
@@ -25,4 +31,24 @@ class Snake:
             new_y = self.segments[i - 1].ycor()
             self.segments[i].goto(new_x, new_y)
         
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+
+
+
