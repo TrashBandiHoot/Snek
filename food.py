9de74a7a9203325eb2a1bdadcc86212f2d230cd3
TrashBandiHoot <112197685+TrashBandiHoot@users.noreply.github.com>
2023-05-09 17:25:27 -0500

    changed food to spawn in middle of grid

--- food.py
+++ food.py
@@ -11,12 +11,14 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        self.x_coordinates = [x for x in range(-280, 280, 20)]
+        self.y_coordinates = [x for x in range(-280, 280, 20)]
+        random_x = random.choice(self.x_coordinates)
+        random_y = random.choice(self.y_coordinates)
         self.goto(random_x, random_y)
 
 
     def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_x = random.choice(self.x_coordinates)
+        random_y = random.choice(self.y_coordinates)
         self.goto(random_x, random_y)
